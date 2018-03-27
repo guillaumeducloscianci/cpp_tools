d14bdfdb4ec0678a68543bc83f70226ac053a781
Guillaume Duclos-Cianci <guillaume.ducloscianci@gmail.com>
2018-03-27 10:57:30 -0400

    Cleaned Project system tests

--- src/project.py
+++ src/project.py
@@ -51,9 +51,9 @@ class Project():
 
     def create_license_header_template(self):
         replacement_rules = {"project_name_": self.path.name, "author_": self.author}
-        destination_path = self.path/".templates"/"license_header.template"
-        content = FileTemplate(File.read(self.create_license_header_path())).instantiate_with(replacement_rules)
-        File.write(destination_path, content)
+        license_header_template = cpp_tools_resources_directory/"license_header.template"
+        content = FileTemplate(File.read(license_header_template)).instantiate_with(replacement_rules)
+        File.write(self.create_license_header_path(), content)
 
     def create_readme_file(self):
         File.write(self.path/"README.md", "## " + self.path.name + "\n")
@@ -75,4 +75,4 @@ class Project():
         return FileTemplate(File.read(self.create_license_header_path())).instantiate_with(replacement_rules)
 
     def create_license_header_path(self):
-        return cpp_tools_resources_directory/"license_header.template"
+        return self.path/".templates"/"license_header.template"
--- src/system_test_project.py
+++ src/system_test_project.py
@@ -11,28 +11,52 @@
 # You should have received a copy of the GNU General Public License along with this program.
 # If not, see <http://www.gnu.org/licenses/>.
 
+from directory import Directory
 from project import Project, ProjectParameters
 from system_test import SystemTest
 
 
-class TestProject(SystemTest):
+class TestProjectBase(SystemTest):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.setup_base()
+
+    @classmethod
+    def tearDownClass(cls):
+        Directory.remove(cls.path)
+
+    @classmethod
+    def setup_base(cls):
+        cls.author = "arbitrary_author"
+        cls.path = SystemTest.get_testing_directory()/"arbitrary_project"
+        cls.parameters = ProjectParameters(cls.path, cls.author)
+
+    def assert_has_a_project_directory_structure(self):
+        for directory in Project.directories:
+            self.assert_directory_exists(self.path/directory)
+
+    def assert_project_directory_contains(self, file_name):
+        self.assert_file_exists(self.path/file_name)
+
+
+class TestProject(TestProjectBase):
 
     def test_create_directory_structure(self):
         self.assert_directory_does_not_exist(self.path)
         Project(self.parameters).create_directory_structure()
         self.assert_has_a_project_directory_structure()
 
-    def setUp(self):
-        self.path = SystemTest.get_testing_directory()/"arbitrary_project"
-        self.author = "arbitrary_author"
-        self.parameters = ProjectParameters(self.path, self.author)
 
-    def assert_has_a_project_directory_structure(self):
-        for directory in Project.directories:
-            self.assert_directory_exists(self.path/directory)
+class TestProjectWithDirectoryStructure(TestProjectBase):
 
+    def test_create_gitignore_file(self):
+        self.project.create_gitignore_file()
+        self.assert_project_directory_contains(".gitignore")
 
-class TestProjectWithDirectoryStructure(SystemTest):
+    def test_create_git_repository(self):
+        self.project.create_git_repository()
+        self.assert_directory_exists(self.path/".git")
 
     def test_create_license_file(self):
         self.project.create_license_file()
@@ -46,30 +70,14 @@ class TestProjectWithDirectoryStructure(SystemTest):
         self.project.create_readme_file()
         self.assert_project_directory_contains("README.md")
 
-    def test_create_gitignore_file(self):
-        self.project.create_gitignore_file()
-        self.assert_project_directory_contains(".gitignore")
-
-    def test_create_git_repository(self):
-        self.project.create_git_repository()
-        self.assert_directory_exists(self.path/".git")
-
     @classmethod
     def setUpClass(cls):
-        cls.setUpClassFromProjectName("arbitrary_project_with_directory_structure")
-
-    def assert_project_directory_contains(self, file_name):
-        self.assert_file_exists(self.path/file_name)
-
-    @classmethod
-    def setUpClassFromProjectName(cls, name):
-        cls.path = SystemTest.get_testing_directory()/name
-        cls.author = "arbitrary_author"
-        cls.project = Project(ProjectParameters(cls.path, cls.author))
+        cls.setup_base()
+        cls.project = Project(cls.parameters)
         cls.project.create_directory_structure()
 
 
-class TestProjectWithLicenseHeaderTemplate(TestProjectWithDirectoryStructure):
+class TestProjectWithLicenseHeaderTemplate(TestProjectBase):
 
     def test_create_top_level_cmakelists(self):
         self.project.create_top_level_cmakelists()
@@ -86,4 +94,7 @@ class TestProjectWithLicenseHeaderTemplate(TestProjectWithDirectoryStructure):
 
     @classmethod
     def setUpClass(cls):
-        cls.setUpClassFromProjectName("arbitrary_project_with_license_header")
+        cls.setup_base()
+        cls.project = Project(cls.parameters)
+        cls.project.create_directory_structure()
+        cls.project.create_license_header_template()
